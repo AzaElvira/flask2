e06295b09997d852338863b7afdafc0c605dadd0
AzaElvira <121398338+AzaElvira@users.noreply.github.com>
2024-01-10 18:20:44 +0400

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,7 @@
+
+class HttpError(Exception):
+
+    def __init__(self, status_code: int, desctiption: str | dict | list):
+
+        self.status_code = status_code
+        self.description = desctiption
